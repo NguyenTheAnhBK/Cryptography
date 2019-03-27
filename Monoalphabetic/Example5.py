8e746d081a0af2f4499dc9d5be70a7f7cbbc2808
ntheanhbk <ntheanhbk@gmail.com>
2019-03-27 10:23:23 +0700

    update

---
+++ Monoalphabetic/Example5.py
@@ -0,0 +1,67 @@
+def module_inverse(k, N = 26): 
+    t1, t2 = 0, 1
+    while k > 0:
+        q = N // k
+        r = N - k*q
+        N, k = k, r
+        t = t1 - t2*q
+        t1, t2 = t2, t
+    if N == 1: 
+        return t1
+    return 0 
+
+def encrypt(plainText, k):
+    cipherText = ""
+    for i in range(len(plainText)):
+        charIndex = ord(plainText[i])
+        if charIndex == 32:
+            cipherText += " "
+        elif charIndex >= 65 and charIndex <= 90:
+            cipherText += chr(((charIndex - 65) * k) % 26 + 65) 
+        else:
+            cipherText += chr(((charIndex - 97) * k) % 26 + 97) 
+    return cipherText
+
+def decrypt(cipherText, k):
+    k = module_inverse(k)
+    if k != 0:
+        plainText = ""
+        for i in range(len(cipherText)):
+            charIndex = ord(cipherText[i])
+            if charIndex == 32:
+                plainText += " "
+            elif charIndex >= 65 and charIndex <= 90:
+                plainText += chr(((charIndex - 65) * k) % 26 + 65) 
+            else:
+                plainText += chr(((charIndex - 97) * k) % 26 + 97) 
+        return plainText
+    return 0 
+
+def multiplicativeCipher_encode():
+    print('Nhập bản rõ: ')
+    plainText = str(input())
+    print("Nhập k: ")
+    k = int(input())
+    while k < 0:
+        print("Nhập lại k: ")
+        k = int(input())
+    cipherText = encrypt(plainText, k)
+    print('Bản mã: %s' % cipherText)
+
+def multiplicativeCipher_decode():
+    print('Nhập bản mã: ')
+    cipherText = str(input())
+    print("Nhập k: ")
+    k = int(input())
+    while k < 0:
+        print("Nhập lại k: ")
+        k = int(input())
+    plainText = decrypt(cipherText, k)
+    if plainText == 0:
+        print('Không thể giải mã !')
+    else:
+        print('Bản mã: %s' % plainText)
+
+# Main
+multiplicativeCipher_encode()
+multiplicativeCipher_decode()
--- Polyalphabetic/HillCiphers.py
+++ Polyalphabetic/HillCiphers.py
@@ -1,27 +1,21 @@
 #-*- coding: utf-8 -*-
+# C = (P*K) mod 26
+# P = (C*K-1) mod 26, K-1 la ma tran nghich dao cua K
 import numpy as np
 
-
 def stringToMatrix(text, m):
-    if len(text) % m  == 0:
-        return np.array([[ord(i) for i in text]]).reshape((-1, m))
-    return np.zeros((2, 2))
+    k = (m - len(text) % m ) % m # Số ký tự cần thêm vào chuỗi
+    while k:
+        text += 'z'
+        k -= 1
+    return np.array([[ord(i) for i in text]]).reshape((-1, m))
 
 def matrixToString(matrix):
     array = np.ravel(matrix)
     text = ""
     for i in array:
         text += chr(i)
-    return text;
-
-def getChildMatrix(matrix, p, q):
-    childMatrix = []
-    for i in range(matrix.shape[0]):
-        for j in range(matrix.shape[1]):
-            if i != p and j != q: 
-                childMatrix.append(matrix[i][j])
-    x = np.asarray(childMatrix).reshape((matrix.shape[0] - 1, -1))
-    return np.asarray(childMatrix).reshape((matrix.shape[0] - 1, -1))
+    return text
 
 def moduleInverse(k, N):
     t1, t2 = 0, 1
@@ -33,62 +27,56 @@ def moduleInverse(k, N):
         t1, t2 = t2, t
     if N == 1:
         return t1
-    return 0
+    return 0 # không tồn tại nghịch đảo module N của k
 
+# hàm lấy ma trận con tại vị trí i, j của ma trận 
+def getChildMatrix(matrix, p, q):
+    childMatrix = []
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            if i != p and j != q:
+                childMatrix.append(matrix[i][j])
+    return np.asarray(childMatrix).reshape((matrix.shape[0]-1, -1))
+
+# hàm tính ma trận nghịch đảo của ma trận K
 def multiplicativeInverse(matrix):
     if matrix.shape[0] != matrix.shape[1]:
         print("Ma tran khoa phai la ma tran vuong")
         return np.zeros_like(matrix)
     
     detMatrix = int(np.linalg.det(matrix) % 26)
-    detInverse = moduleInverse(detMatrix, 26)
-    if detInverse != 0:
+    detMatrixInverse = moduleInverse(detMatrix, 26)
+
+    if detMatrix != 0:
         inverseMatrix = np.zeros_like(matrix)
         for i in range(matrix.shape[0]):
             for j in range(matrix.shape[1]):
-                x = int(np.linalg.det(getChildMatrix(matrix, i, j)))
-                inverseMatrix[i][j] = (-1)**(i+j) * round(np.linalg.det(getChildMatrix(matrix, i, j)))
-        return (detInverse * np.transpose(inverseMatrix)) % 26
+                x = np.linalg.det(getChildMatrix(matrix, i, j))
+                inverseMatrix[i][j] = (-1)**(i+j) * round(x)
+        return (detMatrixInverse * np.transpose(inverseMatrix)) % 26
     return np.zeros_like(matrix)
 
 def hillCipherEncrypt(plainText, k):
     if k.shape[0] != k.shape[1]:
-        print("Khoa k phai la ma tran vuong")
-    else:
-        matrixPlainText = stringToMatrix(plainText, k.shape[0])
-        if matrixPlainText.all() == 0:
-            print("Khoa k khong hop le!")
-        else:
-            cipherText = (matrixPlainText - 65 ).dot(k) % 26
-            cipherText = matrixToString(cipherText + 65)
-            return cipherText
-    return 0
+        print("Ma tran khoa phai la ma tran vuong")
+        return 0
+    matrixPlainText = stringToMatrix(plainText, k.shape[0])
+    cipherText = (matrixPlainText - 97).dot(k) % 26
+    cipherText = matrixToString(cipherText + 97)
+    return cipherText
 
 def hillCipherDecrypt(cipherText, k):
     k_inv = multiplicativeInverse(k)
     if k_inv.all() == 0:
-        print("Khong the giai ma")
+        print("khong the giai ma voi khoa k")
         return 0
     matrixCipherText = stringToMatrix(cipherText, k.shape[0])
-    if matrixCipherText.all() == 0:
-        print("Khoa k khong hop le!")
-        return 0
-    plainText = (matrixCipherText - 65).dot(k_inv) % 26
-    plainText = matrixToString(plainText + 65)
+    plainText = (matrixCipherText - 97).dot(k_inv) % 26
+    plainText = matrixToString(plainText + 97)
     return plainText
-# plainText = "abc"
-# k = np.array([[6, 24, 1], [13, 16, 10], [20, 17, 15]])
-# print(matrixToString(k))
+
 k = np.array([[3, 3], [2, 5]])
-# k_inv = multiplicativeInverse(k)
-# print((stringToMatrix("HELP", 2) - 65) % 26)
-# cipherText1 = (stringToMatrix("HELP", 2) - 65).dot(k) % 26
-# # x = (stringToMatrix("ACT") - 65).reshape((-1, 1))
-# # cipherText2 = k.dot(x) % 26
-# cipherText1 = matrixToString(cipherText1 + 65)
-# # print(matrixToString(cipherText2 + 65))
-# plainText1 = (stringToMatrix(cipherText1, 2)  - 65).dot(k_inv) % 26
-# print(matrixToString(plainText1 + 65))
-cipherText = hillCipherEncrypt("HELP", k)
+cipherText = hillCipherEncrypt("help", k)
+
 print(cipherText)
 print(hillCipherDecrypt(cipherText, k))
